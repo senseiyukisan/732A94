41b76a19d59224c740fee3950093d2db39a3e403
Yuki <yuki@Yukis-MacBook-Pro-2.local>
2020-09-09 20:35:46 +0200

    added data to project

---
+++ R/wiki_graph.r
@@ -0,0 +1,14 @@
+#' @title wiki_graph data
+#' 
+#' @description wiki_graph: DataFrame containing three columns (v1, v2, w) and 18 entries.
+#' @docType data
+#' @format The \code{data.frame} contains 3 variables:
+#' \describe{
+#'   \item{v1}{nodes}
+#'   \item{v2}{nodes}
+#'   \item{w}{weights between the nodes}
+#' }
+#' @references \url{http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'
+#'
+"wiki_graph"
