186cddee119e2248ecfb53be2521908f838357b0
nicta839 <nicolas.juha.taba@gmail.com>
2020-09-11 21:45:23 +0200

    changes to djikstra

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -6,20 +6,22 @@
 #' @references \url{https://en.wikipedia.org/wiki/Dijkstras_algorithm} and \url{https://www.youtube.com/watch?v=Ww4nU_NcAIQ}
 #' @export
 dijkstra <- function(graph, init_node){
-  #assert dataframe of the correct form
+  #assert
   stopifnot(init_node %in% graph[["v1"]])
   stopifnot(length(graph) == 3)
   stopifnot(is.numeric(unlist(graph[[1]])) && is.numeric(unlist(graph[[2]])) && is.numeric(unlist(graph[[3]])))
   stopifnot(length(graph[[1]]) == length(graph[[2]]) && length(graph[[1]]) == length(graph[[3]]))
   
-  #initial setup of vectors and unique nodes visited
+  #initial vectors and unique nodes visited
   nodes <- c(unique(graph[[1]]))
+  # print(nodes)
   nodes_to_visit <- nodes
+  nodes_to_visit_keep <- nodes
   distance <- numeric(length = length(nodes))
   distance[] <- Inf
   visited <- c()
   
-  #set up of adjacency matrix
+  #adjacency matrix
   adj_matrix <- matrix(Inf, nrow = length(nodes), ncol = length(nodes))
   colnames(adj_matrix) <- nodes
   rownames(adj_matrix) <- nodes
@@ -30,21 +32,23 @@ dijkstra <- function(graph, init_node){
   diag(adj_matrix) <- 0
   
   #matrix loop/djikstra's algorithm
-  #initialize distance and visited nodes
   distance[init_node] <- 0
-  #algorithm
+ 
   while(length(nodes_to_visit != 0)){
-    u <- which.min(distance)
+    u <- which.min(distance[nodes_to_visit])
+    u_actual <- nodes_to_visit[u]
     for (v in nodes_to_visit){
-      alt <- distance[u] + adj_matrix[v,u]
+      alt <- distance[u_actual] + adj_matrix[u_actual,v]
       if (alt < distance[v]){
         distance[v] <- alt
       }
     }
-    visited <- c(visited, u)
-    nodes_to_visit <- nodes_to_visit[-u] #removes the node just visited from being considered
+    visited <- c(visited, u_actual)
+    nodes_to_visit <- nodes_to_visit[-u] 
   }
   #return distance vector
+  # print(length(nodes_to_visit))
+  print(visited)
   return(distance)
 }
 wiki_graph <- data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6), v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5), w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
